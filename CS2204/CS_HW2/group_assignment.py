055526105de4ca696e307e8897164b3056c7b4e8
Kai <kdmalc@gmail.com>
2021-09-05 16:11:22 -0500

    Completed HW2

--- CS2204/CS_HW2/group_assignment.py
+++ CS2204/CS_HW2/group_assignment.py
@@ -2,39 +2,53 @@
 # -*- coding: utf-8 -*-
 """Assign students to synchronous lecture groups"""
 
-__author__ = ""
+__author__ = "malcolkd"
 
 from random import sample
 
 
-def read_classroll(filename = "classroll.txt"):
+def read_classroll(filename="classroll.txt"):
     """Read classroll database file and return a list of students."""
     with open(filename) as datafile:
-       students = [line.strip() for line in datafile]
+        students = [line.strip() for line in datafile]
 
-    retrun students
+    return students
+
 
 def assign_groups(students, n_groups=3):
     """Return the group assigment (list of lists) based on random sampling"""
-    groups=[]
-    group_size = len (students) // n_groups
-    #Random shuffle to avoid selection bias
+    groups = []
+    group_size = len(students) // n_groups
+
+    # Random shuffle to avoid selection bias
     shuffled = sample(students, len(students))
+
     group_begin, group_end = 0, group_size
     for i_group in range(n_groups):
         group = shuffled[group_begin:group_end]
         groups.append(group)
-        group_begin, group_end = group_end,group_end + group_size
+
+        missing_students = shuffled[group_end:]
+
+        group_begin, group_end = group_end, group_end + group_size
+
+    idx = 0
+    for student in missing_students:
+        groups[idx].append(student)
+        idx += -2 if idx == 2 else 1
 
     return groups
+
+
 def main():
     days = "Monday", "Wednesday", "Friday"
     groups = assign_groups(read_classroll(), len(days))
-    for day, group in zip(days, groups)
+
+    for day, group in zip(days, groups):
         print(f"\n{day}")
-        for student in sorted(  group):
+        for student in (group):
             print(f"\t{student}")
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()
