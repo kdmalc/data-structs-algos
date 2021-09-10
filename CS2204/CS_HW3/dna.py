9334338a18e7c0a7a8b8ea5b31f041e0bad3e60f
Kai <kdmalc@gmail.com>
2021-09-09 22:17:03 -0500

    Progress on HW3

--- CS2204/CS_HW3/dna.py
+++ CS2204/CS_HW3/dna.py
@@ -2,60 +2,90 @@
 # -*- coding: utf-8 -*-
 """DNA Analysis"""
 
-__author__ = ""
+__author__ = "malcolkd"
+
+
+from math import floor
 
 
 class Codon:
     """Amino-acid encoding with three nucleotides"""
 
     def __init__(self, bases):
-        pass
+        self.bases = bases.upper()
+        # Something about raising exceptions if you get bad input
+        """
+        try:
+            assert len(bases) == 3
+            assert type(bases) ==  str
+            acceptable_bases = ['A', 'C', 'G', 'T']
+            assert bases[0].upper() in acceptable_bases
+
+        except TypeError:
+            print("Error: bases is NoneType")
+        """
 
     def __str__(self):
         """Convert to string enclosed in square brackets.
         E.g. [GCT]
         """
-        pass
+        return self.bases
 
     def __eq__(self, other):
         """Compare if two Codon objects are equal (same sequence of bases)"""
-        pass
+        return self.bases == other.bases
 
     def transcribe(self):
         """Return a string of transcribed bases enclosed in angle brackets.
 
         E.g. <GCU>
         """
-        pass
+        return f"<{self.bases}>"
 
 
 class Gene:
     """Protein encoding with a sequence of codons"""
 
     def __init__(self, seq):
-        pass
+        self.seq = seq.upper()
+        while len(self.seq) % 3 > 0:
+            self.seq = self.seq[0:-1]
+
+        third_length = floor(len(self.seq)/3)
+        seq_hold = [0] * third_length
+        for idx in range(third_length):
+            seq_hold[idx] = (str(self.seq[3*idx]) + str(self.seq[3*idx+1])
+                             + str(self.seq[3*idx+2]))
+        self.seq = seq_hold
 
     def __str__(self):
         """Convert to string using a sequence of codons (in square brackets).
 
         E.g. [GCT][GGC]...
         """
-        pass
+        # Double check that this string doesn't start/end with "[[...]]"
+        # Return this as a string...
+        return [f"[{str(ele)}]" for ele in self.seq]
 
     def transcribe(self):
-        """Return a string of transcribed codons (in anle brackets).
+        """Return a string of transcribed codons (in angle brackets).
 
         E.g. <GCU><GGC>...
         """
-        pass
+        # Double check that this string doesn't start/end with "[[...]]"
+        # Return this as a string...
+        return [f"<{str(ele)}>" for ele in self.seq]
 
     def __contains__(self, codon):
         """Check if the gene seuence contains the given codon"""
-        pass
+        return (True if sum([1 for ele in self.seq
+                             if str(codon).upper() == ele]) > 0 else False)
 
     def gc_content(self):
         """Return the fraction of G and C bases relative to all bases"""
-        pass
+        # This is not good enough, as a codon could be just G/Cs
+        return ((sum([1 for codon in self.seq if "GC" in codon.upper()]) > 0)
+                / len(self.seq))
 
 
 if __name__ == "__main__":
