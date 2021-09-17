a69eaee24519205643b3cc9ad9db8e4d6e22f4ee
Kai <kdmalc@gmail.com>
2021-09-17 10:31:36 -0500

    Updated HW4

--- CS2204/CS_HW4/chemistry.py
+++ CS2204/CS_HW4/chemistry.py
@@ -19,7 +19,7 @@ def load_elements(filename):
     with open(filename) as db:
         for line in db:
             # line is a single string of from the file
-            ele_params = line.split()
+            ele_params = line.strip().split()
             elements.append(Element(int(ele_params[0]), ele_params[1],
                                     ele_params[2], float(ele_params[3])))
 
@@ -54,6 +54,10 @@ def element_by_name(name):
     Name matching is case-insensitive.
     Return None if element is not found.
     """
+    if not type(name) is str:
+        print("Please enter a string!")
+        return None
+
     for elem in elements:
         if elem.name.upper() == name.upper():
             return elem
@@ -62,6 +66,11 @@ def element_by_name(name):
 
 
 def get_full_number(idx, formula):
+    """Return the full number (as an int) taken from a string
+    E.g. if the string is "O123", this function retrieves "123"
+
+    Returns 0 if it is detected that there is no num
+    """
     num_str = ""
     while formula[idx].isdigit():
         num_str = num_str + formula[idx]
@@ -76,8 +85,14 @@ def get_full_number(idx, formula):
 
 def compound_elements(formula, count_instances=False):
     """Return the list of elements in a given compound formula."""
+    if ((type(formula) is not str) or (formula[0] == formula[0].lower())
+            or (not formula)):
+        print("Invalid input!")
+        return None
+
     if count_instances is False:
-        form = ''.join([letter for letter in formula if not letter.isdigit()])
+        form = ''.join([letter for letter in formula
+                        if not letter.isdigit()])
         # Assuming form will be: (CAP lower) (CAP) (CAP lower) (CAP)
         my_elems = []
         for char in form:
@@ -89,8 +104,7 @@ def compound_elements(formula, count_instances=False):
                 raise "Unexpected type"
         # Assuming that these are indeed the elements
         elem_set = set()
-        for elem in my_elems:
-            elem_set.add(element_by_symbol(elem))
+        [elem_set.add(element_by_symbol(elem)) for elem in my_elems]
         return list(elem_set)
     else:
         my_elems = []
@@ -116,8 +130,10 @@ def compound_elements(formula, count_instances=False):
 def compound_weight(formula):
     """Return the total weight of a given compound formula."""
     elem_list = compound_elements(formula, count_instances=True)
-    weight = sum([ele.weight for ele in elem_list])
-    return weight
+    if elem_list is None:
+        return None
+    else:
+        return sum([ele.weight for ele in elem_list])
 
 
 if __name__ == "__main__":
@@ -125,9 +141,19 @@ if __name__ == "__main__":
 
     # Feel free to use (umcomment) these examples to try out your code
 
-    # print("The 17. element:", element_by_number(17).name)
-    # print("The name of Ge:", element_by_symbol("Ge").name)
-    # print("The weight of Oxygen:", element_by_name("Oxygen").weight)
+    print("The 17. element:", element_by_number(17).name)
+    print("The 123412 element: (True if invalid) ",
+          element_by_number(123412) is None)
+    print("The name of Ge:", element_by_symbol("Ge").name)
+    print("The name of asdfasdf: (True if invalid) ",
+          element_by_symbol("asdfasdf") is None)
+    print("The weight of Oxygen:", element_by_name("Oxygen").weight)
 
-    # print("The elements in salt:", compound_elements("NaCl"))
-    # print("The molecular weight of Caffeine", compound_weight("C8H10N4O2"))
+    print("The elements in salt:", compound_elements("NaCl"))
+    print("The elements in salt:", compound_elements("aNCl"))
+    print("The elements in salt:", compound_elements("12"))
+    print("The elements in salt:", compound_elements(12))
+    print("The molecular weight of Caffeine", compound_weight("C8H10N4O2"))
+    print("The molecular weight of Caffeine", compound_weight("c"))
+    print("The molecular weight of Caffeine", compound_weight("1738"))
+    print("The molecular weight of Caffeine", compound_weight(1738))
